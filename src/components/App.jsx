29d704dd71e116bf2195d9bcac46a80889452d51
León Cantillo <leonjob13@gmail.com>
2024-01-04 11:49:08 -0500

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import '../styles/global.css'
+import Layout from '../containers/Layout'
+import Login from '../containers/Login'
 
 const App = () => {
     return (
-        <h1>Hola Mundo!</h1>
+        <Layout>
+            <Login />
+        </Layout>
     )
 }
 
