473caef666cf7aeeed40efbb09551a575c6f17cc
León Cantillo <leonjob13@gmail.com>
2024-01-04 12:12:43 -0500

    add new container RecoveryPassword

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import '../styles/global.css'
 import Layout from '../containers/Layout'
 import Login from '../containers/Login'
+import RecoveryPassword from '../containers/RecoveryPassword'
 
 const App = () => {
     return (
         <Layout>
             <Login />
+            <RecoveryPassword />
         </Layout>
     )
 }
