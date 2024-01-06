4ecfa16d6afbedea164a8f2bffb9bf6428ddf717
León Cantillo <leonjob13@gmail.com>
2024-01-06 16:28:02 -0500

    Including alias & fixing visible icons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import ProductList from '../containers/ProductList'
-import '../styles/Home.scss'
+import ProductList from '@containers/ProductList'
+import '@styles/Home.scss'
+import Header from '@components/Header'
 
 const Home = () => {
     return (
         <div className='Home'>
+            <Header />
             <h1>React Shop</h1>
             <ProductList />
         </div>
--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import '../styles/SendEmail.scss'
+import '@styles/SendEmail.scss'
+
+import iconEmail from '@icons/email.svg'
 
 const SendEmail = () => {
 	return (
@@ -9,7 +11,7 @@ const SendEmail = () => {
 				<h1 className="title">Email has been sent!</h1>
 				<p className="subtitle">Please check your inbox for instructions on how to reset the password</p>
 				<div className="email-image">
-					<img src="./icons/email.svg" alt="email" />
+					<img src={iconEmail} alt="email" />
 				</div>
 				<button className="primary-button login-button">Login</button>
 				<p className="resend">
