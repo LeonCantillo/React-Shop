3a0147a1bb4532572bb6cf9daaf9e5f25ecfca8a
León Cantillo <leonjob13@gmail.com>
2024-01-07 17:51:10 -0500

    toggle UserMenu and fixing styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
+import UserMenu from '@components/UserMenu.jsx'
 import '@styles/Header.scss'
 
-import menu from '@icons/icon_menu.svg'
+import menuHamburguer from '@icons/icon_menu.svg'
 import logo from '@logos/logo_yard_sale.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false)
+  
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
     return (
         <header>
           <nav className='header'>
-            <img src={menu} alt='menu' className='menu' />
+            <img src={menuHamburguer} alt='menuHamburguer' className='menuHamburguer' />
           
             <div className='navbar-left'>
               <img src={logo} alt='logo' className='logo' />
@@ -37,13 +43,17 @@ const Header = () => {
           
             <div className='navbar-right'>
               <ul>
-                <li className='navbar-email'>platzi@example.com</li>
+                <li className='navbar-email'
+                onClick={handleToggle}>
+                platzi@example.com
+                </li>
                 <li className='navbar-shopping-cart'>
                   <img src={shoppingCart} alt='shopping cart' />
                   <div>2</div>
                 </li>
               </ul>
             </div>
+            {toggle && <UserMenu />}
           </nav>
         </header>
     )
