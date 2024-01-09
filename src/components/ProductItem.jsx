c8ed112b9c221d1464aa8c8870e77b48bbed0479
León Cantillo <leonjob13@gmail.com>
2024-01-09 12:08:12 -0500

    fetch products & add alias: @hooks

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,16 +3,16 @@ import '@styles/ProductItem.scss'
 
 import addToCart from '@icons/bt_add_to_cart.svg'
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
     return (
-        <div class="product-card">
-            <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-            <div class="product-info">
+        <div className="product-card">
+            <img src={product.images[0]} alt={product.title} />
+            <div className="product-info">
                 <div>
-                    <p>$120,00</p>
-                    <p>Bike</p>
+                    <p>${product.price}</p>
+                    <p>{product.title}</p>
                 </div>
-                <figure>
+                <figure onClick={handleClick}>
                     <img src={addToCart} alt="" />
                 </figure>
             </div>
