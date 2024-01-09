4dc22ec3686d041310c07cba05c778038716f7ec
León Cantillo <leonjob13@gmail.com>
2024-01-09 14:44:47 -0500

    delete Button component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import Button from '../components/Button'
-import '../styles/404.scss'
+import '@styles/404.scss'
 
 const NotFound = () => {
-    const navigate = useNavigate()
-    const redirectToHomeButton = () => {
-        navigate('/')
-    }
-
     return (
-        <div className="notfound-container">
-            <h4 className="title-notfound">404</h4>
+        <div className='notfound-container'>
+            <h4 className='title-notfound'>404</h4>
             <p>NO ENCONTRADO</p>
-            <Button type={2} functionToEjecute={redirectToHomeButton}>Volver</Button>
+            <a href='/' className='buttonStyles type-2'>Volver</a>
         </div>
     )
 }
