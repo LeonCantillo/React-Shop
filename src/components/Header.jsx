f22276fb2ddf16449625968b2cb2afb4f355ff93
León Cantillo <leonjob13@gmail.com>
2024-01-10 09:19:46 -0500

    item counter in the shopping cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, { useState, useContext } from 'react'
+import AppContext from '@context/AppContext'
 import UserMenu from '@components/UserMenu.jsx'
 import '@styles/Header.scss'
 
@@ -7,56 +8,57 @@ import logo from '@logos/logo_yard_sale.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 const Header = () => {
+  const { state } = useContext(AppContext)
   const [toggle, setToggle] = useState(false)
-  
   const handleToggle = () => {
     setToggle(!toggle)
   }
-    return (
-        <header>
-          <nav className='header container'>
-            <img src={menuHamburguer} alt='menuHamburguer' className='menuHamburguer' />
-          
-            <div className='navbar-left'>
-              <img src={logo} alt='logo' className='logo' />
-              <ul>
-                <li>
-                  <a href='/'>All</a>
-                </li>
-                <li>
-                  <a href='/'>Clothes</a>
-                </li>
-                <li>
-                  <a href='/'>Electronics</a>
-                </li>
-                <li>
-                  <a href='/'>Furnitures</a>
-                </li>
-                <li>
-                  <a href='/'>Toys</a>
-                </li>
-                <li>
-                  <a href='/'>Others</a>
-                </li>
-              </ul>
-            </div>
-          
-            <div className='navbar-right'>
-              <ul>
-                <li className='navbar-email'
-                onClick={handleToggle}>
-                platzi@example.com
-                </li>
-                <li className='navbar-shopping-cart'>
-                  <img src={shoppingCart} alt='shopping cart' />
-                  <div>2</div>
-                </li>
-              </ul>
-            </div>
-            {toggle && <UserMenu />}
-          </nav>
-        </header>
-    )
+  return (
+    <header>
+      <nav className='header container'>
+        <img src={menuHamburguer} alt='menuHamburguer' className='menuHamburguer' />
+
+        <div className='navbar-left'>
+          <img src={logo} alt='logo' className='logo' />
+          <ul>
+            <li>
+              <a href='/'>All</a>
+            </li>
+            <li>
+              <a href='/'>Clothes</a>
+            </li>
+            <li>
+              <a href='/'>Electronics</a>
+            </li>
+            <li>
+              <a href='/'>Furnitures</a>
+            </li>
+            <li>
+              <a href='/'>Toys</a>
+            </li>
+            <li>
+              <a href='/'>Others</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className='navbar-right'>
+          <ul>
+            <li className='navbar-email'
+              onClick={handleToggle}>
+              platzi@example.com
+            </li>
+            <li className='navbar-shopping-cart'>
+              <img src={shoppingCart} alt='shopping cart' />
+              {state.cart.length > 0 ?
+              <div>{state.cart.length}</div> : null}
+            </li>
+          </ul>
+        </div>
+        {toggle && <UserMenu />}
+      </nav>
+    </header>
+  )
 }
 
 export default Header
