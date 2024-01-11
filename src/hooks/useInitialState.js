8fb66a04ee709079e83f5c02ea35e45f0a074eff
León Cantillo <leonjob13@gmail.com>
2024-01-11 16:52:08 -0500

    [ADD] removeFromCart function

--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -6,15 +6,32 @@ const initialState = {
 
 const useInitialState = () => {
     const [state, setState] = useState(initialState)
+    
     const addToCart = (payload) => {
         setState({
             ...state,
             cart: [...state.cart, payload]
         })
     }
+    
+    const removeFromCart = (payload) => {
+        const indexToRemove = state.cart.findIndex(item => item.id === payload.id);
+      
+        if (indexToRemove !== -1) {
+      
+          setState({
+            ...state,
+            cart: 
+            [...state.cart.slice(0, indexToRemove), ...state.cart.slice(indexToRemove + 1)]
+          });
+        }
+      }
+      
+
     return {
         state,
-        addToCart
+        addToCart,
+        removeFromCart
     }
 }
 
